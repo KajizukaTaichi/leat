c9803574999764277bba97ffca257728e3b78c74
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 15:05:14 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -7,11 +7,10 @@ impl Expr {
             let [name, tokens] = [tokens.first()?.to_vec(), tokens.get(1..)?.concat()];
             let tokens: Vec<&[Token]> = tokens.split(|x| *x == Token::In).collect();
             let [value, after_expr] = [tokens.first()?.to_vec(), tokens.get(1..)?.concat()];
-            Some(Expr::Let(
-                Box::new(Expr::parse(name)?),
-                Box::new(Expr::parse(value)?),
-                Box::new(Expr::parse(after_expr)?),
-            ))
+            let name = Box::new(Expr::parse(name)?);
+            let value = Box::new(Expr::parse(value)?);
+            let after_expr = Box::new(Expr::parse(after_expr)?);
+            Some(Expr::Let(name, value, after_expr))
         } else {
             None
         }
