bdde3e150dfdc4e11268484990186a3b5887f62f
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 15:06:35 +0900

    Update parse.rs

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 use crate::*;
 
 impl Expr {
-    fn parse(tokens: Vec<Token>) -> Option<Expr> {
+    pub fn parse(tokens: Vec<Token>) -> Option<Expr> {
         if let Token::Let = tokens.first()? {
             let tokens: Vec<&[Token]> = tokens.split(|x| *x == Token::Assign).collect();
             let [name, tokens] = [tokens.first()?.to_vec(), tokens.get(1..)?.concat()];
@@ -11,6 +11,15 @@ impl Expr {
             let value = Box::new(Expr::parse(value)?);
             let after_expr = Box::new(Expr::parse(after_expr)?);
             Some(Expr::Let(name, value, after_expr))
+        } else if let Token::If = tokens.first()? {
+            let tokens: Vec<&[Token]> = tokens.split(|x| *x == Token::Then).collect();
+            let [cond, tokens] = [tokens.first()?.to_vec(), tokens.get(1..)?.concat()];
+            let tokens: Vec<&[Token]> = tokens.split(|x| *x == Token::Else).collect();
+            let [then, els] = [tokens.first()?.to_vec(), tokens.get(1..)?.concat()];
+            let cond = Box::new(Expr::parse(cond)?);
+            let then = Box::new(Expr::parse(then)?);
+            let els = Box::new(Expr::parse(els)?);
+            Some(Expr::If(cond, then, els))
         } else {
             None
         }
