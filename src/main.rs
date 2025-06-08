d15549bb54d6f11c96f89f51e340932db891f7de
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 21:13:24 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod eval;
 mod fmt;
 mod lexer;
 mod parse;
+mod stdlib;
 
 use indexmap::IndexMap;
-pub use lexer::*;
 use rustyline::{DefaultEditor, error::ReadlineError};
+pub use {lexer::*, stdlib::stdlib};
 
 fn main() {
     println!("Hello, world!");
@@ -77,131 +78,4 @@ pub enum LeatError {
     InvalidBind(Expr),
     InvalidArg(Expr),
     InvalidOperation,
-}
-
-fn stdlib() -> Env {
-    macro_rules! curry_2arg {
-        ($processing: expr) => {
-            Value::Lambda(Lambda::BuiltIn(
-                |a, mut env| {
-                    Ok(Value::Lambda(Lambda::BuiltIn(
-                        |b, env| {
-                            let a = env.get("a").unwrap().clone();
-                            $processing(a, b)
-                        },
-                        {
-                            env.insert(String::from("a"), a);
-                            env
-                        },
-                    )))
-                },
-                IndexMap::new(),
-            ))
-        };
-    }
-    IndexMap::from([
-        (
-            String::from("+"),
-            curry_2arg!(|a, b| {
-                match [a, b] {
-                    [Value::Number(a), Value::Number(b)] => Ok(Value::Number(a + b)),
-                    [Value::String(a), Value::String(b)] => Ok(Value::String(a + &b)),
-                    _ => Err(LeatError::InvalidOperation),
-                }
-            }),
-        ),
-        (
-            String::from("-"),
-            curry_2arg!(|a, b| {
-                match [a, b] {
-                    [Value::Number(a), Value::Number(b)] => Ok(Value::Number(a - b)),
-                    _ => Err(LeatError::InvalidOperation),
-                }
-            }),
-        ),
-        (
-            String::from("*"),
-            curry_2arg!(|a, b| {
-                match [a, b] {
-                    [Value::Number(a), Value::Number(b)] => Ok(Value::Number(a * b)),
-                    [Value::String(a), Value::Number(b)] => Ok(Value::String(a.repeat(b as usize))),
-                    _ => Err(LeatError::InvalidOperation),
-                }
-            }),
-        ),
-        (
-            String::from("/"),
-            curry_2arg!(|a, b| match [a, b] {
-                [Value::Number(a), Value::Number(b)] => Ok(Value::Number(a / b)),
-                _ => Err(LeatError::InvalidOperation),
-            }),
-        ),
-        (
-            String::from("=="),
-            curry_2arg!(|a, b| Ok(Value::Bool(a == b))),
-        ),
-        (
-            String::from(">"),
-            curry_2arg!(|a, b| {
-                match [a, b] {
-                    [Value::Number(a), Value::Number(b)] => Ok(Value::Bool(a > b)),
-                    [Value::String(a), Value::String(b)] => Ok(Value::Bool(a > b)),
-                    _ => Err(LeatError::InvalidOperation),
-                }
-            }),
-        ),
-        (
-            String::from("<"),
-            curry_2arg!(|a, b| match [a, b] {
-                [Value::Number(a), Value::Number(b)] => Ok(Value::Bool(a < b)),
-                [Value::String(a), Value::String(b)] => Ok(Value::Bool(a < b)),
-                _ => Err(LeatError::InvalidOperation),
-            }),
-        ),
-        (
-            String::from("&"),
-            curry_2arg!(|a, b| match [a, b] {
-                [Value::Bool(a), Value::Bool(b)] => Ok(Value::Bool(a & b)),
-                _ => Err(LeatError::InvalidOperation),
-            }),
-        ),
-        (
-            String::from("|"),
-            curry_2arg!(|a, b| match [a, b] {
-                [Value::Bool(a), Value::Bool(b)] => Ok(Value::Bool(a | b)),
-                _ => Err(LeatError::InvalidOperation),
-            }),
-        ),
-        (
-            String::from("cast"),
-            curry_2arg!(|a, b| {
-                match [a, b] {
-                    [Value::Number(a), Value::Type(Type::String)] => {
-                        Ok(Value::String(a.to_string()))
-                    }
-                    [Value::String(a), Value::Type(Type::Number)] => {
-                        if let Ok(n) = a.parse::<f64>() {
-                            Ok(Value::Number(n))
-                        } else {
-                            Err(LeatError::InvalidOperation)
-                        }
-                    }
-                    _ => Err(LeatError::InvalidOperation),
-                }
-            }),
-        ),
-        (
-            String::from("typeof"),
-            Value::Lambda(Lambda::BuiltIn(
-                |a, _| match a {
-                    Value::Number(_) => Ok(Value::Type(Type::Number)),
-                    Value::String(_) => Ok(Value::Type(Type::String)),
-                    Value::Bool(_) => Ok(Value::Type(Type::Bool)),
-                    Value::Lambda(_) => Ok(Value::Type(Type::Lambda)),
-                    Value::Type(_) => Ok(Value::Type(Type::Kind)),
-                },
-                IndexMap::new(),
-            )),
-        ),
-    ])
 }
