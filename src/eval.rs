732fd4a802fb5584884a5c2e69d09134d6b623b5
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 14:38:20 +0900

    Update eval.rs

--- src/eval.rs
+++ src/eval.rs
@@ -5,6 +5,18 @@ impl Expr {
         match self {
             Expr::Literal(value) => Some(value.to_owned()),
             Expr::Variable(name) => env.get(name).cloned(),
+            Expr::Call(func, arg) => {
+                let Value::Lambda(lambda) = func.eval(env)? else {
+                    return None;
+                };
+                match lambda {
+                    Lambda::BuiltIn(body, func_env) => body(arg.eval(env)?, func_env),
+                    Lambda::UserDefined(arg_name, body, mut func_env) => {
+                        func_env.insert(arg_name, arg.eval(env)?);
+                        body.eval(&mut func_env)
+                    }
+                }
+            }
             Expr::Let(name, value, after_expr) => match *name.clone() {
                 Expr::Variable(name) => {
                     env.insert(name.to_owned(), value.eval(env)?);
