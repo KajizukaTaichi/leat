61f28b09189c3af655191e52a13a3efd6ff6f440
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 15:46:42 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub use lexer::*;
 
 fn main() {
     println!("Hello, world!");
-    dbg!(run(r#"let b = + 1 in b 2"#));
+    dbg!(run(r#"let inc n = + n 1 in inc (inc 5)"#));
 }
 
 fn run(code: &str) -> Option<Value> {
