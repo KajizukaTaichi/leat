a01a2a8f1801583c8d00341f7f06e0c77bd3021e
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-08 17:23:36 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -42,9 +42,9 @@ impl Token {
             Token::Type(Type::Lambda)
         } else if token == "#kind" {
             Token::Type(Type::Kind)
-        } else if token == "λ" {
+        } else if token == "\\" {
             Token::Lambda
-        } else if token == "." {
+        } else if token == "->" {
             Token::Dot
         } else if let Ok(b) = token.parse::<bool>() {
             Token::Bool(b)
@@ -65,53 +65,38 @@ pub fn lex(input: &str) -> Option<Vec<Token>> {
     let mut current_token = String::new();
     let mut in_parentheses: usize = 0;
     let mut in_quote = false;
-    let mut is_escape = false;
 
     for c in input.chars() {
-        if is_escape {
-            current_token.push(match c {
-                'n' => '\n',
-                't' => '\t',
-                'r' => '\r',
-                _ => c,
-            });
-            is_escape = false;
-        } else {
-            match c {
-                '(' | '{' | '[' if !in_quote => {
-                    current_token.push(c);
-                    in_parentheses += 1;
-                }
-                ')' | '}' | ']' if !in_quote => {
-                    current_token.push(c);
-                    in_parentheses.checked_sub(1).map(|x| in_parentheses = x);
-                }
-                '"' | '\'' | '`' => {
-                    in_quote = !in_quote;
-                    current_token.push(c);
-                }
-                '\\' if in_quote => {
-                    current_token.push(c);
-                    is_escape = true;
-                }
-                other => {
-                    if other.is_whitespace() && !in_quote {
-                        if in_parentheses != 0 {
-                            current_token.push(c);
-                        } else if !current_token.is_empty() {
-                            tokens.push(Token::new(current_token.clone())?);
-                            current_token.clear();
-                        }
-                    } else {
+        match c {
+            '(' | '{' | '[' if !in_quote => {
+                current_token.push(c);
+                in_parentheses += 1;
+            }
+            ')' | '}' | ']' if !in_quote => {
+                current_token.push(c);
+                in_parentheses.checked_sub(1).map(|x| in_parentheses = x);
+            }
+            '"' | '\'' | '`' => {
+                in_quote = !in_quote;
+                current_token.push(c);
+            }
+            other => {
+                if other.is_whitespace() && !in_quote {
+                    if in_parentheses != 0 {
                         current_token.push(c);
+                    } else if !current_token.is_empty() {
+                        tokens.push(Token::new(current_token.clone())?);
+                        current_token.clear();
                     }
+                } else {
+                    current_token.push(c);
                 }
             }
         }
     }
 
     // Syntax error check
-    if is_escape || in_quote || in_parentheses != 0 {
+    if in_quote || in_parentheses != 0 {
         return None;
     }
     if !current_token.is_empty() {
