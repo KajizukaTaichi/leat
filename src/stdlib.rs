a46285ff497130902e21a40ed79ae9b7c88be519
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-10 01:02:08 +0900

    Update stdlib.rs

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -133,20 +133,6 @@ pub fn stdlib() -> Env {
             )),
         ),
         (
-            String::from("eval"),
-            Value::Lambda(Lambda::BuiltIn(
-                |a, _| match a {
-                    Value::Number(_) => Ok(Value::Type(Type::Number)),
-                    Value::String(_) => Ok(Value::Type(Type::String)),
-                    Value::Bool(_) => Ok(Value::Type(Type::Bool)),
-                    Value::Array(_) => Ok(Value::Type(Type::Array)),
-                    Value::Lambda(_) => Ok(Value::Type(Type::Lambda)),
-                    Value::Type(_) => Ok(Value::Type(Type::Kind)),
-                },
-                IndexMap::new(),
-            )),
-        ),
-        (
             String::from("car"),
             Value::Lambda(Lambda::BuiltIn(
                 |array, _| {
