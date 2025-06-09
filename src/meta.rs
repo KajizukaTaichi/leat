35e857496c7bca3c73556730e5d25ec77a9aa217
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-09 15:15:17 +0900

    Update meta.rs

--- src/meta.rs
+++ src/meta.rs
@@ -7,7 +7,29 @@ impl Expr {
             to.clone()
         } else {
             match self {
-                Expr::Call(func, args) => Expr::Call(func.replace(from, to), args),
+                Expr::Let(name, value, expr) => Expr::Let(
+                    Box::new(name.replace(from, to)),
+                    Box::new(value.replace(from, to)),
+                    Box::new(expr.replace(from, to)),
+                ),
+                Expr::Call(func, args) => Expr::Call(
+                    Box::new(func.replace(from, to)),
+                    Box::new(args.replace(from, to)),
+                ),
+                Expr::If(cond, then, els) => Expr::If(
+                    Box::new(cond.replace(from, to)),
+                    Box::new(then.replace(from, to)),
+                    Box::new(els.replace(from, to)),
+                ),
+                Expr::Variable(name) => Expr::Variable(name.to_owned()),
+                Expr::Literal(Value::Lambda(Lambda::UserDefined(arg, body, env))) => {
+                    Expr::Literal(Value::Lambda(Lambda::UserDefined(
+                        arg.to_string(),
+                        Box::new(body.replace(from, to)),
+                        env.clone(),
+                    )))
+                }
+                Expr::Literal(value) => Expr::Literal(value.to_owned()),
             }
         }
     }
