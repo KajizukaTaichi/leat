5cbbe1686fa351de0aa9efc5b3e2d90feb4d63e2
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-10 13:26:38 +0900

    Create repl.rs

---
+++ src/repl.rs
@@ -0,0 +1,55 @@
+use crate::*;
+use colored::*;
+use crossterm::{
+    cursor::MoveUp,
+    execute,
+    terminal::{Clear, ClearType},
+};
+use rustyline::{DefaultEditor, error::ReadlineError};
+use std::io::stdout;
+
+pub fn repl() {
+    println!("{} REPL", "Leat".blue().bold().underline());
+    let mut rl = DefaultEditor::new().unwrap();
+    let mut buf = String::new();
+    let mut env = stdlib();
+    let mut line = 1;
+
+    loop {
+        match rl.readline("> ") {
+            Ok(code) => {
+                buf.push_str(&code);
+                buf.push_str("\n");
+                rl.add_history_entry(code).unwrap();
+                if let Some(Some(ast)) = lex(&buf).map(|x| Expr::parse(x)) {
+                    match ast.eval(&mut env) {
+                        Ok(res) => println!("{} {res}", "=".green().bold()),
+                        Err(err) => println!("{} {err}", "Error!".red().bold()),
+                    }
+                    buf.clear();
+                    line = 1
+                } else {
+                    line += 1
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                clear_up(line);
+                buf.clear();
+                line = 1
+            }
+            Err(ReadlineError::Eof) => {
+                println!("Bye");
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
+fn clear_up(lines: usize) {
+    let mut stdout = stdout();
+    for _ in 0..lines {
+        execute!(stdout, MoveUp(1)).unwrap();
+        execute!(stdout, Clear(ClearType::CurrentLine)).unwrap();
+    }
+}
