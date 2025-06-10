dccc3edab501fbe723abb7948dd3b52677d027ed
梶塚太智 <kajizukataichi@outlook.jp>
2025-06-10 15:06:35 +0900

    Update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,5 @@
+use crossterm::cursor;
+
 use crate::*;
 
 pub fn lex(input: &str) -> Option<Vec<Token>> {
@@ -64,6 +66,57 @@ pub fn lex(input: &str) -> Option<Vec<Token>> {
                         current_token.push(c);
                     }
                 }
+            }
+        }
+    }
+
+    // Syntax error check
+    if is_escape || in_quote || in_parentheses != 0 {
+        return None;
+    }
+    if !current_token.is_empty() {
+        tokens.push(Token::new(current_token.clone())?);
+        current_token.clear();
+    }
+
+    Some(tokens)
+}
+
+pub fn f_string(input: &str) -> Option<Vec<Token>> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut current_token = String::new();
+    let mut in_parentheses: usize = 0;
+
+    let mut is_escape = false;
+
+    for c in input.chars() {
+        if is_escape {
+            current_token.push(match c {
+                'n' => '\n',
+                't' => '\t',
+                'r' => '\r',
+                _ => c,
+            });
+            is_escape = false;
+        } else {
+            is_escape = false;
+            match c {
+                '{' if in_parentheses == 0 => {
+                    tokens.push(Token::String(current_token));
+                    current_token.clear();
+                    in_parentheses += 1;
+                }
+                '}' => {
+                    in_parentheses.checked_sub(1).map(|x| in_parentheses = x);
+                    if in_parentheses == 0 {
+                        tokens.append(&mut lex(&current_token)?);
+                    }
+                }
+                '`' => {
+                    current_token.push(c);
+                    is_escape = true;
+                }
+                _ => current_token.push(c),
             }
         }
     }
